7f0ad3a9589fff0a118b36d2fb27d1a98f2fd9b9
sillas <sillasstore09@gmail.com>
2025-11-12 20:45:08 -0300

    adicionar getters e setters, metodos: emprestar e devolver

--- .idea/src/main/java/com/biblioteca/Livro.java
+++ .idea/src/main/java/com/biblioteca/Livro.java
@@ -1,9 +1,9 @@
 package com.biblioteca;
 
 public class Livro {
-    String titulo;
-    String autor;
-    String identificador;
+    private String titulo;
+    private String autor;
+    private String identificador;
     private int totalDeCopias;
     private int totalEmprestado;
 
@@ -15,5 +15,59 @@ public class Livro {
         this.totalEmprestado = 0;
     }
 
+    public int getCopiasDisponiveis(){
+        return this.totalDeCopias - this.totalEmprestado;
+    }
 
+    public String getAutor() {
+        return autor;
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+
+    public void setTotalDeCopias(int totalDeCopias) {
+        this.totalDeCopias = totalDeCopias;
+    }
+
+    public int getTotalEmprestado() {
+        return totalEmprestado;
+    }
+
+    public void setTotalEmprestado(int totalEmprestado) {
+        this.totalEmprestado = totalEmprestado;
+    }
+
+    public boolean emprestar() {
+        if (getCopiasDisponiveis() > 0) {
+            this.totalEmprestado ++;
+            return true;
+        } else {
+            System.out.println("todas a copias do livro: " + titulo + " foram emprestadas :(");
+            return false;
+        }
+
+    }
+
+    public boolean devolver() {
+        if (this.totalEmprestado > 0 ) {
+            this.totalEmprestado--;
+            return true;
+        } else {
+            System.out.println("você não pegou este livro em nossa biblioteca :)");
+            return false;
+        }
+    }
 }
