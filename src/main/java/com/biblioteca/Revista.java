f539b92e1c125ae6476d0fce581b2e36f5ae5529
sillas <sillasstore09@gmail.com>
2025-11-13 21:03:38 -0300

    adicionar a classe revista e a classe biblioteca

---
+++ src/main/java/com/biblioteca/Revista.java
@@ -0,0 +1,18 @@
+package com.biblioteca;
+
+public class Revista extends ItemBiblioteca {
+
+    private int edicao;
+
+    public Revista (String titulo, String identificador, int edicao) {
+        super(titulo, identificador);
+        this.edicao = edicao;
+    }
+
+    public int calcularPrazoDevolucaoDias(){
+        return 7;
+    }
+    public int getEdicao(){
+        return edicao;
+    }
+}
